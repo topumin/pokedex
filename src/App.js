cf864edc609ce75baddd33d2c2fc858f38143f85
DESKTOP-UTRB32B\nonstop <pumin.wongsiri@gmail.com>
2020-12-04 14:33:00 +0700

    mock up frontend completed

--- src/App.js
+++ src/App.js
@@ -65,14 +65,272 @@ export default function App() {
               </div>
             </div>
 
-            
-                  
+            <div className="modal-list-item">
+              <div className="add">
+                ADD
+              </div>
+              <div className="card">
+                <img src="https://images.pokemontcg.io/ex8/98.png" alt="pokemon" />
+              </div>
+              <div className="card-detail">
+                <h3>Deoxys ex</h3>
+                <div className="type">
+                  <label>HP</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 350}}></div>
+                  </div>
+                </div>
+                <div className="type">
+                  <label>STR</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 175}}></div>
+                  </div>
+                </div>
+                <div className="type">
+                  <label>WEAK</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 350}}></div>
+                  </div>
+                </div>
+                <div className="emoji">
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                </div>
+              </div>
+            </div>
+
+            <div className="modal-list-item">
+              <div className="add">
+                ADD
+              </div>
+              <div className="card">
+                <img src="https://images.pokemontcg.io/ex8/98.png" alt="pokemon" />
+              </div>
+              <div className="card-detail">
+                <h3>Deoxys ex</h3>
+                <div className="type">
+                  <label>HP</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 350}}></div>
+                  </div>
+                </div>
+                <div className="type">
+                  <label>STR</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 175}}></div>
+                  </div>
+                </div>
+                <div className="type">
+                  <label>WEAK</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 350}}></div>
+                  </div>
+                </div>
+                <div className="emoji">
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                </div>
+              </div>
+            </div>
+
+            <div className="modal-list-item">
+              <div className="add">
+                ADD
+              </div>
+              <div className="card">
+                <img src="https://images.pokemontcg.io/ex8/98.png" alt="pokemon" />
+              </div>
+              <div className="card-detail">
+                <h3>Deoxys ex</h3>
+                <div className="type">
+                  <label>HP</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 350}}></div>
+                  </div>
+                </div>
+                <div className="type">
+                  <label>STR</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 175}}></div>
+                  </div>
+                </div>
+                <div className="type">
+                  <label>WEAK</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 350}}></div>
+                  </div>
+                </div>
+                <div className="emoji">
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                </div>
+              </div>
+            </div>
+
+            <div className="modal-list-item">
+              <div className="add">
+                ADD
+              </div>
+              <div className="card">
+                <img src="https://images.pokemontcg.io/ex8/98.png" alt="pokemon" />
+              </div>
+              <div className="card-detail">
+                <h3>Deoxys ex</h3>
+                <div className="type">
+                  <label>HP</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 350}}></div>
+                  </div>
+                </div>
+                <div className="type">
+                  <label>STR</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 175}}></div>
+                  </div>
+                </div>
+                <div className="type">
+                  <label>WEAK</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 350}}></div>
+                  </div>
+                </div>
+                <div className="emoji">
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                </div>
+              </div>
+            </div>
+
+            <div className="modal-list-item">
+              <div className="add">
+                ADD
+              </div>
+              <div className="card">
+                <img src="https://images.pokemontcg.io/ex8/98.png" alt="pokemon" />
+              </div>
+              <div className="card-detail">
+                <h3>Deoxys ex</h3>
+                <div className="type">
+                  <label>HP</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 350}}></div>
+                  </div>
+                </div>
+                <div className="type">
+                  <label>STR</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 175}}></div>
+                  </div>
+                </div>
+                <div className="type">
+                  <label>WEAK</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 350}}></div>
+                  </div>
+                </div>
+                <div className="emoji">
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                </div>
+              </div>
+            </div>
+
+            <div className="modal-list-item">
+              <div className="add">
+                ADD
+              </div>
+              <div className="card">
+                <img src="https://images.pokemontcg.io/ex8/98.png" alt="pokemon" />
+              </div>
+              <div className="card-detail">
+                <h3>Deoxys ex</h3>
+                <div className="type">
+                  <label>HP</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 350}}></div>
+                  </div>
+                </div>
+                <div className="type">
+                  <label>STR</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 175}}></div>
+                  </div>
+                </div>
+                <div className="type">
+                  <label>WEAK</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 350}}></div>
+                  </div>
+                </div>
+                <div className="emoji">
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                </div>
+              </div>
+            </div>
+
+            <div className="modal-list-item">
+              <div className="add">
+                ADD
+              </div>
+              <div className="card">
+                <img src="https://images.pokemontcg.io/ex8/98.png" alt="pokemon" />
+              </div>
+              <div className="card-detail">
+                <h3>Deoxys ex</h3>
+                <div className="type">
+                  <label>HP</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 350}}></div>
+                  </div>
+                </div>
+                <div className="type">
+                  <label>STR</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 175}}></div>
+                  </div>
+                </div>
+                <div className="type">
+                  <label>WEAK</label> 
+                  <div className="bar">
+                    <div className="result" style={{width: 350}}></div>
+                  </div>
+                </div>
+                <div className="emoji">
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                  <img src={emoji} alt="happiness"/>
+                </div>
+              </div>
+            </div>      
+
           </div>
 
         </form>
         </Modal>
         <navbar className="header">My Pokedex</navbar>
         <div className="content">
+         
           <div className="list-item">
             <div className="cancel">
               X
@@ -110,7 +368,190 @@ export default function App() {
             </div>
           </div>
           
-            
+          <div className="list-item">
+            <div className="cancel">
+              X
+            </div>
+            <div className="card">
+              <img src="https://images.pokemontcg.io/ex8/98.png" alt="pokemon"/>
+            </div>
+            <div className="card-detail">
+              <h3>Deoxys ex</h3>
+              <div className="type">
+                <label>HP</label> 
+                <div className="bar">
+                  <div className="result" style={{width: 160}}></div>
+                </div>
+              </div>
+              <div className="type">
+                <label>STR</label> 
+                <div className="bar">
+                  <div className="result" style={{width: 80}}></div>
+                </div>
+              </div>
+              <div className="type">
+                <label>WEAK</label> 
+                <div className="bar">
+                  <div className="result" style={{width: 160}}></div>
+                </div>
+              </div>
+              <div className="emoji">
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+              </div>
+            </div>
+          </div>
+
+          <div className="list-item">
+            <div className="cancel">
+              X
+            </div>
+            <div className="card">
+              <img src="https://images.pokemontcg.io/ex8/98.png" alt="pokemon"/>
+            </div>
+            <div className="card-detail">
+              <h3>Deoxys ex</h3>
+              <div className="type">
+                <label>HP</label> 
+                <div className="bar">
+                  <div className="result" style={{width: 160}}></div>
+                </div>
+              </div>
+              <div className="type">
+                <label>STR</label> 
+                <div className="bar">
+                  <div className="result" style={{width: 80}}></div>
+                </div>
+              </div>
+              <div className="type">
+                <label>WEAK</label> 
+                <div className="bar">
+                  <div className="result" style={{width: 160}}></div>
+                </div>
+              </div>
+              <div className="emoji">
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+              </div>
+            </div>
+          </div>
+
+          <div className="list-item">
+            <div className="cancel">
+              X
+            </div>
+            <div className="card">
+              <img src="https://images.pokemontcg.io/ex8/98.png" alt="pokemon"/>
+            </div>
+            <div className="card-detail">
+              <h3>Deoxys ex</h3>
+              <div className="type">
+                <label>HP</label> 
+                <div className="bar">
+                  <div className="result" style={{width: 160}}></div>
+                </div>
+              </div>
+              <div className="type">
+                <label>STR</label> 
+                <div className="bar">
+                  <div className="result" style={{width: 80}}></div>
+                </div>
+              </div>
+              <div className="type">
+                <label>WEAK</label> 
+                <div className="bar">
+                  <div className="result" style={{width: 160}}></div>
+                </div>
+              </div>
+              <div className="emoji">
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+              </div>
+            </div>
+          </div>
+
+          <div className="list-item">
+            <div className="cancel">
+              X
+            </div>
+            <div className="card">
+              <img src="https://images.pokemontcg.io/ex8/98.png" alt="pokemon"/>
+            </div>
+            <div className="card-detail">
+              <h3>Deoxys ex</h3>
+              <div className="type">
+                <label>HP</label> 
+                <div className="bar">
+                  <div className="result" style={{width: 160}}></div>
+                </div>
+              </div>
+              <div className="type">
+                <label>STR</label> 
+                <div className="bar">
+                  <div className="result" style={{width: 80}}></div>
+                </div>
+              </div>
+              <div className="type">
+                <label>WEAK</label> 
+                <div className="bar">
+                  <div className="result" style={{width: 160}}></div>
+                </div>
+              </div>
+              <div className="emoji">
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+              </div>
+            </div>
+          </div>
+
+                    <div className="list-item">
+            <div className="cancel">
+              X
+            </div>
+            <div className="card">
+              <img src="https://images.pokemontcg.io/ex8/98.png" alt="pokemon"/>
+            </div>
+            <div className="card-detail">
+              <h3>Deoxys ex</h3>
+              <div className="type">
+                <label>HP</label> 
+                <div className="bar">
+                  <div className="result" style={{width: 160}}></div>
+                </div>
+              </div>
+              <div className="type">
+                <label>STR</label> 
+                <div className="bar">
+                  <div className="result" style={{width: 80}}></div>
+                </div>
+              </div>
+              <div className="type">
+                <label>WEAK</label> 
+                <div className="bar">
+                  <div className="result" style={{width: 160}}></div>
+                </div>
+              </div>
+              <div className="emoji">
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+                <img src={emoji} alt="happiness"/>
+              </div>
+            </div>
+          </div>    
           
         </div>
         <navbar className="footer">
